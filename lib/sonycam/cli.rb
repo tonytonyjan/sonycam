1e5db8f8c45caa5002ac2c910cee75b85902c000
Tony Jian <tonytonyjan@gmail.com>
2014-08-03 00:32:00 +0800

    auto scanning

--- lib/sonycam/cli.rb
+++ lib/sonycam/cli.rb
@@ -16,22 +16,30 @@ module Sonycam
 
     desc 'list QUERY', 'List all API or search'
     def list query
-      api = API.new DeviceDescription.new(DD_PATH).api_url(:camera)
-      puts api.request(:getAvailableApiList)['result'].first.select{|method| method =~ /#{query}/i }
+      puts api_client.request(:getAvailableApiList)['result'].first.select{|method| method =~ /#{query}/i }
     end
 
     desc 'api METHOD PARAMS', 'Send API request'
     def api method, *params
-      api = API.new DeviceDescription.new(DD_PATH).api_url(:camera)
-      jj api.request(method, *params)
+      jj api_client.request(method, *params)
     end
 
     desc 'liveview', 'Start Liveview and output to STDOUT'
     def liveview
-      api = API.new DeviceDescription.new(DD_PATH).api_url(:camera)
-      liveview_url = api.request('startLiveview')['result'][0]
+      liveview_url = api_client.request('startLiveview')['result'][0]
       Liveview.stream(liveview_url) do |packet|
         puts packet[:payload_data][:jpeg_data]
+      end
+    end
+
+    no_tasks do
+      def api_client
+        return @api_client if @api_client.is_a? API
+        unless File.exist?(DD_PATH)
+          puts "Can not find #{DD_PATH}, start scanning..."
+          invoke :scan, []
+        end
+        @api_client = API.new DeviceDescription.new(DD_PATH).api_url(:camera)
       end
     end
   end
